3b7eae087b123a6f8bf42a6a6b055274b3908c76
KzQ-96 <k.humienny1996@wp.pl>
2020-04-28 22:03:23 +0200

    random4

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 
 const questionText = document.querySelector('.question-Text');
 const optionBox = document.querySelector('.option-box');
-const questionIndex = 0;
+const currentQuestionNum=document.querySelector('.current-question-num');
+const answerDescription=document.querySelector('.answer-description');
+const nextQuestionBtn=document.querySelector('.next-question-btn');
+const correctAnswer=document.querySelector('.correct-answers');
+const seeResultBtn=document.querySelector('.see-result-btn');
+let questionIndex = 0;
+let score = 0;
+let number = 0;
+let myArray=[];
 
 // questions and options and answer and answer description 
 // array of object
@@ -10,7 +18,6 @@ myApp = [
         question: "Ile kcal ma 10 gramów białka",
         options: ["1", "10", "40", "100"],
         answer:2,
-        description:''
     },
     {
         question: "Sen nie wpływa na proces redukcji",
@@ -36,40 +43,100 @@ myApp = [
     }
 
 ]
-
+// tworzenie ptania
 function load(){
+    number++;
     questionText.innerHTML=myApp[questionIndex].question;
     createOptions();
+    scoreBoard();
+    currentQuestionNum.innerHTML=number + " / " + myApp.length;
 }
-
+// tworzenie odpowiedzi
 function createOptions(){
+    optionBox.innerHTML="";
+    let animationDealy=0.2;
     for (let i=0; i<myApp[questionIndex].options.length; i++) {
         const option = document.createElement('div');
         option.innerHTML=myApp[questionIndex].options[i];
         option.classList.add('option');
         option.id = i;
+        animationDealy=animationDealy+0.2;
+        option.style.animationDelay=animationDealy + 's';
         option.setAttribute('onclick', 'check(this)')
         optionBox.appendChild(option);
     }
 }
-
+// funkcja odpowiedzi wywołana kliknieciem
 function check(ele) {
     const id = ele.id;
     if(id==myApp[questionIndex].answer) {
         ele.classList.add('correct');
+        score++;
+        scoreBoard();
     } else {
         ele.classList.add('wrong');
     }
-
-    disableOptions()
+    disableOptions();
+    showAnswerDescription();
+    showNextQuestionBtn();
+      if(number == myApp.length){
+        quizOver();
+    }
 }
 
+generateRandomQuestion(){
+    const randomNumber=Math.floor(Math.random() * myApp.length);
+    if(myArray.length == 0){
+        questionIndex=randomNumber;
+    }
+}
+
+// zablokowanie dalszego wybierania odpowiedzi
 function disableOptions(){
     for(let i=0; i<optionBox.children.length; i++) {
         optionBox.children[i].classList.add("already-answered")
     }
 }
+// wywołanie opisu jeżeli jest zawarty
+function showAnswerDescription() {
+    if(typeof myApp[questionIndex].description !== 'undefined') {
+    answerDescription.classList.add('show');
+    answerDescription.innerHTML=myApp[questionIndex].description;
+    }
+}
+
+function hideAnswerDescription() {
+    answerDescription.classList.remove('show');
+    answerDescription.innerHTML="";
+}
+// wywołanie przycisku nastepne pytanie
+function showNextQuestionBtn() {
+    nextQuestionBtn.classList.add('show');
+}
+
+function hideNextQuestionBtn() {
+    nextQuestionBtn.classList.remove('show');
+}
+// wywołania wyniku
+function scoreBoard() {
+    correctAnswer.innerHTML=score;
+}
+// wywołanie następne pytanie
+nextQuestionBtn.addEventListener('click', nextQuestion);
+
+function nextQuestion(){
+    questionIndex++;
+    load();
+    hideNextQuestionBtn();
+    hideAnswerDescription();
+}
+
+function quizOver(){
+    nextQuestionBtn.classList.remove("show");
+    seeResultBtn.classList.add("show");
+}
 
 window.onload=()=> {
     load();
+    generateRandomQuestion();
 }
