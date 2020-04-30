5b7034df7966ee62afb36c9e531ab98a34fc0928
KzQ-96 <k.humienny1996@wp.pl>
2020-04-30 12:31:00 +0200

    QuizEnd

--- script.js
+++ script.js
@@ -11,60 +11,183 @@ const timeUpText=document.querySelector('.time-up-text');
 const quizHomeBox=document.querySelector('.quiz-home-box');
 const quizBox=document.querySelector('.quiz-box');
 const quizOverBox=document.querySelector('.quiz-over-box');
+const startAgainQuizBtn=document.querySelector('.start-again-quiz-btn');
+const goHomeBtn=document.querySelector('.go-home-btn');
+const categoryBox=document.querySelector('.category-box');
+const categoryText=document.querySelector('.category-text');
+// const startQuizBtn=document.querySelector('.start-quiz-btn');
 let attempt = 0;
 let questionIndex = 0;
 let score = 0;
 let number = 0;
 let myArray=[];
 let interval;
+let categoryIndex;
 
 // questions and options and answer and answer description 
 // array of object
 myApp = [
-    {
-        question: "Ile kcal ma 10 gramów białka",
-        options: ["1", "10", "40", "100"],
-        answer:2,
-    },
-    {
-        question: "Sen nie wpływa na proces redukcji",
-        options: ["prawda", "fałsz"],
-        answer:1,
-        description:'Wiele osób spi po 8h'
-    },
-    {
-        question: "Triceps posiada ...",
-        options: ["1 głowe", "2 głowy", "3 głowy", "4 głowy"],
-        answer:2,
-    },
-    {
-        question: "Można tyć nic nie jedząc",
-        options: ["prawda", "fałsz",],
-        answer:1,
-        description:'Na prawde nie można'
-    },
-    {
-        question: "Czym jest kreatyna",
-        options: ["suplementem", "sterydem", "kwasem tłuszczowym", "hormonem"],
-        answer:0,
-    }
+{
+    category:"Dietetyka",
+    quizWrap:[
+           {
+            question: "Ile kcal ma 10 gramów białka",
+            options: ["1", "10", "40", "100"],
+            answer:2,
+        },
+        {
+            question: "Sen nie wpływa na proces redukcji",
+            options: ["prawda", "fałsz"],
+            answer:1,
+            description:'Wiele osób spi po 8h'
+        },
+        {
+            question: "Triceps posiada ...",
+            options: ["1 głowe", "2 głowy", "3 głowy", "4 głowy"],
+            answer:2,
+        },
+        {
+            question: "Można tyć nic nie jedząc",
+            options: ["prawda", "fałsz",],
+            answer:1,
+            description:'Na prawde nie można'
+        },
+        {
+            question: "Czym jest kreatyna",
+            options: ["suplementem", "sterydem", "kwasem tłuszczowym", "hormonem"],
+            answer:0,
+        }
+        ]
+},
+{
+    category:"Dietetyka1",
+    quizWrap:[
+           {
+            question: "Ile kcal ma 10 gramów białka",
+            options: ["1", "10", "40", "100"],
+            answer:2,
+        },
+        {
+            question: "Sen nie wpływa na proces redukcji",
+            options: ["prawda", "fałsz"],
+            answer:1,
+            description:'Wiele osób spi po 8h'
+        },
+        {
+            question: "Triceps posiada ...",
+            options: ["1 głowe", "2 głowy", "3 głowy", "4 głowy"],
+            answer:2,
+        },
+        {
+            question: "Można tyć nic nie jedząc",
+            options: ["prawda", "fałsz",],
+            answer:1,
+            description:'Na prawde nie można'
+        },
+        {
+            question: "Czym jest kreatyna",
+            options: ["suplementem", "sterydem", "kwasem tłuszczowym", "hormonem"],
+            answer:0,
+        }
+        ]
+},
+{
+    category:"Dietetyka2",
+    quizWrap:[
+           {
+            question: "Ile kcal ma 10 gramów białka",
+            options: ["1", "10", "40", "100"],
+            answer:2,
+        },
+        {
+            question: "Sen nie wpływa na proces redukcji",
+            options: ["prawda", "fałsz"],
+            answer:1,
+            description:'Wiele osób spi po 8h'
+        },
+        {
+            question: "Triceps posiada ...",
+            options: ["1 głowe", "2 głowy", "3 głowy", "4 głowy"],
+            answer:2,
+        },
+        {
+            question: "Można tyć nic nie jedząc",
+            options: ["prawda", "fałsz",],
+            answer:1,
+            description:'Na prawde nie można'
+        },
+        {
+            question: "Czym jest kreatyna",
+            options: ["suplementem", "sterydem", "kwasem tłuszczowym", "hormonem"],
+            answer:0,
+        }
+        ]
+},
+{
+    category:"Dietetyka3",
+    quizWrap:[
+           {
+            question: "Ile kcal ma 10 gramów białka",
+            options: ["1", "10", "40", "100"],
+            answer:2,
+        },
+        {
+            question: "Sen nie wpływa na proces redukcji",
+            options: ["prawda", "fałsz"],
+            answer:1,
+            description:'Wiele osób spi po 8h'
+        },
+        {
+            question: "Triceps posiada ...",
+            options: ["1 głowe", "2 głowy", "3 głowy", "4 głowy"],
+            answer:2,
+        },
+        {
+            question: "Można tyć nic nie jedząc",
+            options: ["prawda", "fałsz",],
+            answer:1,
+            description:'Na prawde nie można'
+        },
+        {
+            question: "Czym jest kreatyna",
+            options: ["suplementem", "sterydem", "kwasem tłuszczowym", "hormonem"],
+            answer:0,
+        }
+        ]
+}
+        ]
 
-]
+function createCategory(){
+    for (let i=0; i< myApp.length; i++){
+        const categoryList=document.createElement('div');
+            categoryList.innerHTML=myApp[i].category;
+            categoryList.setAttribute('data-id', i);
+            categoryList.setAttribute('onclick', 'selectCategory(this)');
+            categoryBox.appendChild(categoryList);
+    }
+}
+function selectCategory(ele){
+    categoryIndex=ele.getAttribute('data-id');
+    categoryText.innerHTML=myApp[categoryIndex].category;
+    quizHomeBox.classList.remove('show');
+    quizBox.classList.add('show');
+    nextQuestion();
+}
 // tworzenie ptania
 function load(){
     number++;
-    questionText.innerHTML=myApp[questionIndex].question;
+    questionText.innerHTML=myApp[categoryIndex].quizWrap[questionIndex].question;
     createOptions();
     scoreBoard();
-    currentQuestionNum.innerHTML=number + " / " + myApp.length;
+    currentQuestionNum.innerHTML=number + " / " + myApp[categoryIndex].quizWrap.length;
 }
 // tworzenie odpowiedzi
 function createOptions(){
     optionBox.innerHTML="";
     let animationDealy=0.2;
-    for (let i=0; i<myApp[questionIndex].options.length; i++) {
+    for (let i=0; i<myApp[categoryIndex].quizWrap[questionIndex].options.length; i++) {
         const option = document.createElement('div');
-        option.innerHTML=myApp[questionIndex].options[i];
+        option.innerHTML=myApp[categoryIndex].quizWrap[questionIndex].options[i];
         option.classList.add('option');
         option.id = i;
         animationDealy=animationDealy+0.2;
@@ -76,14 +199,14 @@ function createOptions(){
 // funkcja odpowiedzi wywołana kliknieciem
 function check(ele) {
     const id = ele.id;
-    if(id==myApp[questionIndex].answer) {
+    if(id==myApp[categoryIndex].quizWrap[questionIndex].answer) {
         ele.classList.add('correct');
         score++;
         scoreBoard();
     } else {
         ele.classList.add('wrong');
         for(let i=0; i<optionBox.children.length; i++){
-            if(optionBox.children[i].id==myApp[questionIndex].answer){
+            if(optionBox.children[i].id==myApp[categoryIndex].quizWrap.answer){
                 optionBox.children[i].classList.add("show-correct");
             }
         }
@@ -94,13 +217,13 @@ function check(ele) {
     showNextQuestionBtn();
     stopTimer();
 
-      if(number == myApp.length){
+      if(number == myApp[categoryIndex].quizWrap.length){
         quizOver();
     }
 }
 
 function generateRandomQuestion(){
-    const randomNumber=Math.floor(Math.random() * myApp.length);
+    const randomNumber=Math.floor(Math.random() * myApp[categoryIndex].quizWrap.length);
     let hitDublicate=0;
     if(myArray.length == 0){
         questionIndex=randomNumber;
@@ -125,14 +248,14 @@ function generateRandomQuestion(){
 function timeIsUp(){
     showTimeUpText();
     for(let i=0; i<optionBox.children.length; i++){
-        if(optionBox.children[i].id==myApp[questionIndex].answer){
+        if(optionBox.children[i].id==myApp[categoryIndex].quizWrap[questionIndex].answer){
             optionBox.children[i].classList.add("show-correct");
         }
     }
     disableOptions();
     showAnswerDescription();
     showNextQuestionBtn();
-    if(number == myApp.length){
+    if(number == myApp[categoryIndex].quizWrap.length){
         quizOver();
       }
 }
@@ -168,9 +291,9 @@ function disableOptions(){
 }
 // wywołanie opisu jeżeli jest zawarty
 function showAnswerDescription() {
-    if(typeof myApp[questionIndex].description !== 'undefined') {
+    if(typeof myApp[categoryIndex].quizWrap[questionIndex].description !== 'undefined') {
     answerDescription.classList.add('show');
-    answerDescription.innerHTML=myApp[questionIndex].description;
+    answerDescription.innerHTML=myApp[categoryIndex].quizWrap[questionIndex].description;
     }
 }
 
@@ -211,11 +334,19 @@ function nextQuestion(){
 }
 
 function quizResult(){
-    document.querySelector('.total-questions').innerHTML=myApp.length;
+    document.querySelector('.total-questions').innerHTML=myApp[categoryIndex].quizWrap.length;
     document.querySelector('.total-attempt').innerHTML=attempt;
     document.querySelector('.total-correct').innerHTML=score;
     document.querySelector('.total-wrong').innerHTML=attempt-score;
-    document.querySelector('.percentage').innerHTML=(score / myApp.length * 100 + '%');
+    document.querySelector('.percentage').innerHTML=(score / myApp[categoryIndex].quizWrap.length * 100).toFixed(2) + '%';
+}
+
+function quizReset(){
+    attempt = 0;
+    questionIndex = 0;
+    score = 0;
+    number = 0;
+    myArray=[];
 }
 
 function quizOver(){
@@ -224,14 +355,32 @@ function quizOver(){
 }
 
 seeResultBtn.addEventListener('click', ()=> {
+    // quizBox.style.display='none';
+    quizBox.classList.remove('show');
     quizOverBox.classList.add('show');
     seeResultBtn.classList.remove('show');
-    quizBox.style.display='none';
     quizResult();
 })
 
+startAgainQuizBtn.addEventListener('click', ()=> {
+    quizOverBox.classList.remove('show');
+    quizBox.classList.add('show');
+    quizReset();
+    nextQuestion();
+})
+
+goHomeBtn.addEventListener('click', ()=>{
+    quizOverBox.classList.remove('show');
+    quizHomeBox.classList.add('show');
+    quizReset();
+})
+
+// startQuizBtn.addEventListener('click', ()=> {
+//     quizHomeBox.classList.remove('show');
+//     quizBox.classList.add('show');
+//     nextQuestion();
+// })
+
 window.onload=()=> {
-    // load();
-    generateRandomQuestion();
-    startTimer();
+    createCategory();
 }
